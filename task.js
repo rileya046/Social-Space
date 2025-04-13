97bd309169f1619e2b9cc8d858f86f0e72d45859
rileya046 <143229160+rileya046@users.noreply.github.com>
2025-04-13 04:36:41 +0000

    the image is opinionated

--- task.js
+++ task.js
@@ -4,7 +4,6 @@ var todoTask = document.getElementById("todoTask");
 
 
 function addTaskFunct() {
-//addTask.addEventListener("click", () => {
     const task = inputTask.value;
     inputTask.value = "";
 
@@ -15,10 +14,7 @@ function addTaskFunct() {
 
     var tempList = document.createElement("li");
 
-    //li.appendChild(document.createTextNode(task));
     todoTask.appendChild(tempList);
-
-    // creates a check box
 
     var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
@@ -33,33 +29,6 @@ function addTaskFunct() {
     tempList.appendChild(checkbox);
     tempList.appendChild(span);
     todoTask.appendChild(tempList);
-    /*const task = inputTask.value;
-
-    const newTask = document.createElement("div");
-    newTask.classList.add("task");
-    newTask.innerHTML = `
-    <p>${task}</p>
-    <button class="complete">Done</button>
-    `;
-
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox"; 
-
-    var span = document.createElement("span");
-    span.textContent = task;
-
-    checkbox.addEventListener("change", function() {
-        span.classList.toggle("completed");
-    });
-
-    const complete = newTask.querySelector(".complete");
-    complete.addEventListener("click", () => {
-        newTask.remove();
-    });
-
-    todoTask.appendChild(newTask);
-    inputTask.value = "";*/
-//}); 
 }  
 
 addTask.addEventListener("click", addTaskFunct);
