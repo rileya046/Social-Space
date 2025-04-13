2032bb1375af9c41a9cf4601ca3f4310b12a3d34
rileya046 <143229160+rileya046@users.noreply.github.com>
2025-04-13 04:13:51 +0000

    transferring my teammate's code to do a different elimination style

--- task.js
+++ task.js
@@ -2,8 +2,38 @@ const inputTask = document.getElementById("taskText");
 const addTask = document.getElementById("add-task");
 var todoTask = document.getElementById("todoTask");
 
-addTask.addEventListener("click", () => {
+
+function addTaskFunct() {
+//addTask.addEventListener("click", () => {
     const task = inputTask.value;
+    inputTask.value = "";
+
+    if (task == ""){
+        alert("Add task here!")
+        return;
+    }
+
+    var tempList = document.createElement("li");
+
+    //li.appendChild(document.createTextNode(task));
+    todoTask.appendChild(tempList);
+
+    // creates a check box
+
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    var span = document.createElement("span");
+    span.textContent = task;
+
+    checkbox.addEventListener("change", function() {
+        span.classList.toggle("completed");
+    });
+
+    tempList.appendChild(checkbox);
+    tempList.appendChild(span);
+    todoTask.appendChild(tempList);
+    /*const task = inputTask.value;
 
     const newTask = document.createElement("div");
     newTask.classList.add("task");
@@ -12,11 +42,24 @@ addTask.addEventListener("click", () => {
     <button class="complete">Done</button>
     `;
 
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox"; 
+
+    var span = document.createElement("span");
+    span.textContent = task;
+
+    checkbox.addEventListener("change", function() {
+        span.classList.toggle("completed");
+    });
+
     const complete = newTask.querySelector(".complete");
     complete.addEventListener("click", () => {
         newTask.remove();
     });
 
     todoTask.appendChild(newTask);
-    inputTask.value = "";
-});
+    inputTask.value = "";*/
+//}); 
+}  
+
+addTask.addEventListener("click", addTaskFunct);
